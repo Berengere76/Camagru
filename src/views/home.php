bf072a6b52b4585f07fb84f84c2e251bf96edacf
blebas <blebas@student.42lehavre.fr>
2025-01-27 17:56:13 +0100

    fefwefwe

---
+++ src/views/home.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'Accueil</title>
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Bienvenue sur Camagru</h1>
+        <nav>
+            <ul>
+                <li><a href="/register">S'inscrire</a></li>
+                <li><a href="/login">Se connecter</a></li>
+                <li><a href="/profile">Mon Profil</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
