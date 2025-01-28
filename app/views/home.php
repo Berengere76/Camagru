94e70f14d47356f6f26cd051e1de0d7991f87533
blebas <blebas@student.42lehavre.fr>
2025-01-28 16:07:00 +0100

    wow

--- app/views/home.php
+++ app/views/home.php
@@ -5,18 +5,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page d'Accueil</title>
-    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
+
 </head>
 <body>
     <header>
         <h1>Bienvenue sur Camagru</h1>
-        <nav>
-            <ul>
-                <li><a href="/register">S'inscrire</a></li>
-                <li><a href="/login">Se connecter</a></li>
-                <li><a href="/profile">Mon Profil</a></li>
-            </ul>
-        </nav>
+        <form action="/login" method="post">
+            <input type="email" name="email" placeholder="Adresse e-mail" required>
+            <input type="password" name="password" placeholder="Mot de passe" required>
+            <button type="submit">Se connecter</button>
+        </form>
+        <p>Pas encore de compte ? <a href="/register">Inscrivez-vous</a></p>
     </header>
+
+    <div class="clouds">
+        <img src="../images/cloud1.png" alt="Nuage 1" class="cloud cloud1">
+        <img src="../images/cloud1.png" alt="Nuage 2" class="cloud cloud2">
+        <img src="../images/cloud1.png" alt="Nuage 3" class="cloud cloud3">
+    </div>
+
+    <div class="star star1"></div>
+    <div class="star star2"></div>
+    <div class="star star3"></div>
+    <div class="star star4"></div>
+    <div class="star star5"></div>
+    <div class="star star6"></div>
+    <div class="star star7"></div>
+    <div class="star star8"></div>
+    <div class="star star9"></div>
+    <div class="star star10"></div>
+
+    <div class="sky">
+        <div class="shooting-star"></div>
+        <div class="shooting-star"></div>
+        <div class="shooting-star"></div>
+    </div>
+
+
 </body>
 </html>
