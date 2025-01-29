2426b23062f1346e4dabf9d385e43f3bdad50db1
Berengere <berengerelebas@hotmail.com>
2025-01-29 14:58:35 +0100

    au revoir les nuages

--- app/views/home.php
+++ app/views/home.php
@@ -5,13 +5,7 @@
     <?php require_once dirname(__DIR__) . '/templates/headerhome.php'; ?>
 
     <main>
-     
-    <div class="clouds">
-        <img src="../images/cloud1.png" alt="Nuage 1" class="cloud cloud1">
-        <img src="../images/cloud1.png" alt="Nuage 2" class="cloud cloud2">
-        <img src="../images/cloud1.png" alt="Nuage 3" class="cloud cloud3">
-    </div>
-    
+
     </main>
 
     <?php require_once dirname(__DIR__) . '/templates/footer.php'; ?>
