5bd7082425ecedcb76671d0f9d6e14121cf111ad
Berengere <berengerelebas@hotmail.com>
2025-01-29 14:58:01 +0100

    hello

--- app/views/home.php
+++ app/views/home.php
@@ -6,6 +6,12 @@
 
     <main>
      
+    <div class="clouds">
+        <img src="../images/cloud1.png" alt="Nuage 1" class="cloud cloud1">
+        <img src="../images/cloud1.png" alt="Nuage 2" class="cloud cloud2">
+        <img src="../images/cloud1.png" alt="Nuage 3" class="cloud cloud3">
+    </div>
+    
     </main>
 
     <?php require_once dirname(__DIR__) . '/templates/footer.php'; ?>
