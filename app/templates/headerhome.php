04187b20bd92b51ca5159aac1525f9f7446bd4d8
Mathis-Levieux <mathis.levieux1@gmail.com>
2025-01-29 14:48:03 +0100

    nouvelles pages

---
+++ app/templates/headerhome.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="header">
+        <div>
+            <img class="logo" src="/images/logo.png" alt="Logo Camagru">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="/views/galerie.php">Galerie</a></li>
+                <li><a href="/views/profil.php">Profil</a></li>
+                <li><a href="/views/login.php">Se déconnecter</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
