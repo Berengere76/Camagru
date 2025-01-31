f2808a043ff16c23cb1ac34954ea6d48a3807d06
blebas <blebas@student.42lehavre.fr>
2025-01-31 16:01:31 +0100

    golbut

--- app/controllers/logout.php
+++ app/controllers/logout.php
@@ -3,6 +3,6 @@ require_once dirname(__DIR__) . '/models/user.php';
 session_start();
 session_unset();
 session_destroy();
-header("Location : login.php");
+header("Location: login.php");
 exit;
 ?>
